49e3873e2ed748ca07b95d2abf91e4c7a821a140
Irina Ftv <irish9sea@yandex.by>
2022-11-12 00:03:23 +0300

    fateeva_hw5_3

--- homework5/task3.py
+++ homework5/task3.py
@@ -1,17 +1,8 @@
-def days_of_the_week(a):
-    return {
-       a == 1: "Monday",
-       a == 2: "Tuesday",
-       a == 3: "Wednesday",
-       a == 4: "Thursday",
-       a == 5: "Friday",
-       a == 6: "Saturday",
-       a == 7: "Sunday"
-    }[True]
-
-
-while True:
-    day = int(input("Enter the number: "))
-    if 1 <= day <= 7:
-        break
-print(days_of_the_week(day))
+print({"1": "Monday",
+       "2": "Tuesday",
+       "3": "Wednesday",
+       "4": "Thursday",
+       "5": "Friday",
+       "6": "Saturday",
+       "7": "Sunday"
+       }[input("Enter the number: ")])
