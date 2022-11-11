05bd64147c7df999202febce868b1ba93a5078d7
Tatiana <tanyapilipshanova@gmail.com>
2022-11-11 12:22:45 +0300

    merge homework5

--- homework5/task3.py
+++ homework5/task3.py
@@ -1,15 +1,15 @@
 a = int(input("Введите число от 1 до 7: "))
-if a==1:
+if a == 1:
     print("Понедельник")
-elif a==2:
+elif a == 2:
     print("Вторник")
-elif a==3:
+elif a == 3:
     print("Среда")
-elif a==4:
+elif a == 4:
     print("Четверг")
-elif a==5:
+elif a == 5:
     print("Пятница")
-elif a==6:
+elif a == 6:
     print("Суббота")
 else:
     print("Воскресенье")
