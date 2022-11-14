616221d8f12aad22bd1e4cd3c1e27a8f3799b9a5
Nastya Hurynovich <nastia.gurinovich96@gmail.com>
2022-11-15 00:15:33 +0300

    Added python_collections homework solutions

---
+++ quiz.py
@@ -0,0 +1,20 @@
+import sys
+
+quiz = {
+    'Пушкин': 'Золотой петушок',
+    'Толстой': 'Война и мир',
+    'Гоголь': 'Вий'
+}
+hello = input("Привет! Хочешь пройти викторину? Yes/No\n")
+if hello.lower() == "yes":
+    right_answers_count = 0
+    for author, title in quiz.items():
+        answer = input(f"Назовите автора произведения {title}: ")
+        if answer.lower() == author.lower():
+            right_answers_count += 1
+            print("Верно!")
+        else:
+            print("Вы ошиблись.")
+    print(f"Ваш результат: {right_answers_count} из {len(quiz)}")
+else:
+    sys.exit()
