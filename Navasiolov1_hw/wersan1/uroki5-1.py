38d52b24f16bc7f4d5422b06551fdff81e412530
iWERSAN <navasiolov.tolia@gmail.com>
2022-11-14 21:12:05 +0300

    Ðuroki5

---
+++ Navasiolov1_hw/wersan1/uroki5-1.py
@@ -0,0 +1,12 @@
+god = 2022
+
+rec = input("\nВведите слова(1): ").capitalize()
+rec2 = input("\nВведите слово(2): ").capitalize()
+rec3 = f"Это ваши слов: {rec} {rec2}"
+
+print(rec3)
+
+data = int(input("Введите год своего рождения:ке "))
+data1 = f"Ваш год рождения: {god - data} "
+
+print(data1)
