cb6845e0d7f50b3f3d9317f70abdd5ce0fedba22
Nastya Hurynovich <nastia.gurinovich96@gmail.com>
2022-11-16 00:02:35 +0300

    SimplшÐified ш'if' clause

--- quiz.py
+++ quiz.py
@@ -6,15 +6,14 @@ quiz = {
     'Гоголь': 'Вий'
 }
 hello = input("Привет! Хочешь пройти викторину? Yes/No\n")
-if hello.lower() == "yes":
-    right_answers_count = 0
-    for author, title in quiz.items():
-        answer = input(f"Назовите автора произведения {title}: ")
-        if answer.lower() == author.lower():
-            right_answers_count += 1
-            print("Верно!")
-        else:
-            print("Вы ошиблись.")
-    print(f"Ваш результат: {right_answers_count} из {len(quiz)}")
-else:
-    sys.exit()
+if hello.lower() != "yes":
+   sys.exit()
+right_answers_count = 0
+for author, title in quiz.items():
+    answer = input(f"Назовите автора произведения {title}: ")
+    if answer.lower() == author.lower():
+        right_answers_count += 1
+        print("Верно!")
+    else:
+        print("Вы ошиблись.")
+print(f"Ваш результат: {right_answers_count} из {len(quiz)}")
