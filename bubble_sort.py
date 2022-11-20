aabfcfab93ebade3209059e941f65543657fb30f
Nastya Hurynovich <nastia.gurinovich96@gmail.com>
2022-11-21 01:35:32 +0300

    Added bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,13 @@
+import numpy
+
+def bubble_sort(numbers_list):
+    for i in range(len(numbers_list) - 1, 0, -1):
+        for j in range(i):
+            if numbers_list[j] > numbers_list[j + 1]:
+                numbers_list[j], numbers_list[j + 1] = numbers_list[j + 1], numbers_list[j]
+    return numbers_list
+
+
+size = int(input("Введите размер списка: "))
+print(bubble_sort(numpy.random.randint(0, 100, size)))
+
