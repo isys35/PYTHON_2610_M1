38a4ea990136578b348a6008b29d278efbc9e58a
iWERSAN <navasiolov.tolia@gmail.com>
2022-11-23 14:39:58 +0300

    fix: wh4

--- Navasiolov_hw/Navasiolov_hw4.py
+++ Navasiolov_hw/Navasiolov_hw4.py
@@ -1,12 +1,12 @@
-print("Зравствуйте, введите имя и пароль!")
-login = input("Имя: ")
-print("\nЗдрвавствуйте ", login)
-p = int(input("\nВведите сумму заказа: "))
-d = p * 0.18
-f = p * 0.04
-g = p + d + f
-print("\nСумма заказа: ", p)
-print("Чайвые: ", d)
-print("Налог: ", f)
-print("\nСумма к оплате: ", g)
-print("Хорошего дня! :)")
+print("Зравствуйте, введите ваше имя! ")
+name = input("Имя: ")
+print("\nЗдрвавствуйте ", name)
+Order = int(input("\nВведите сумму заказа:" ))
+Tea = Order * 0.18
+Tax = Order * 0.04
+Amount = Order + Tea + Tax
+print("\nСумма заказа: ", Order)
+print("Чайвые: ", Tea)
+print("Налог: ", Tax)
+print("\nСумма к оплате: ", Amount)
+print("Хорошего дня! ", name)
