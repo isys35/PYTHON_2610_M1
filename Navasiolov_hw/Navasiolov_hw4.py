e844771c0f6000ff515199fe292f0abc3ca2f955
iWERSAN <navasiolov.tolia@gmail.com>
2022-11-24 17:30:27 +0300

    fix_2: hw4

--- Navasiolov_hw/Navasiolov_hw4.py
+++ Navasiolov_hw/Navasiolov_hw4.py
@@ -1,12 +1,12 @@
 print("Зравствуйте, введите ваше имя! ")
 name = input("Имя: ")
 print("\nЗдрвавствуйте ", name)
-Order = int(input("\nВведите сумму заказа:" ))
-Tea = Order * 0.18
-Tax = Order * 0.04
-Amount = Order + Tea + Tax
-print("\nСумма заказа: ", Order)
-print("Чайвые: ", Tea)
-print("Налог: ", Tax)
-print("\nСумма к оплате: ", Amount)
+order = int(input("\nВведите сумму заказа:"))
+tea = order * 0.18
+tax = order * 0.04
+amount = order + tea + tax
+print("\nСумма заказа: ", order)
+print("Чайвые: ", tea)
+print("Налог: ", tax)
+print("\nСумма к оплате: ", amount)
 print("Хорошего дня! ", name)
