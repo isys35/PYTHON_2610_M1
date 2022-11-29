1a1c2c8a91bc2a345c991dba3978d6a1a4f966b2
iWERSAN <navasiolov.tolia@gmail.com>
2022-11-29 23:01:50 +0300

    hw5

---
+++ navasiolov_hw5/an_5-1.py
@@ -0,0 +1,15 @@
+number_1 = int(input("\n Введите число: "))
+number_2 = int(input(" Введите число: "))
+number_3 = int(input(" Введите число: "))
+if number_1 + number_3:
+    print(" Треугольник ")
+else:
+    print(" Пока не ")
+if number_2 - number_3:
+    print(" Этот")
+else:
+    print(" Не ")
+if number_1 / number_2 == number_3:
+    print(" Существует")
+else:
+    print(" Не доработан! ")
