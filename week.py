6185f61199c5fbf06c9be68f038aff1b28ebc03f
IvanZhodik <118998353+IvanZhodik@users.noreply.github.com>
2022-11-30 11:08:40 +0200

    Все исправил

--- week.py
+++ week.py
@@ -2,22 +2,17 @@ x = int(input("Введите число от 1 до 7:"))
 if x == 1 or 2 or 3 or 4 or 5 or 6 or 7:
     if x == 1:
         print("Понедельник")
-    else:
-        if x == 2:
-            print("Вторник")
-        else:
-            if x == 3:
-                print("Среда")
-            else:
-                if x == 4:
-                    print("Четверг")
-                else:
-                    if x == 5:
-                        print("ПЯТНИЦА")
-                    else:
-                        if x == 6:
-                            print("Суббота")
-                        else:
-                             print("Воскресенье")
+    elif x == 2:
+        print("Вторник")
+    elif x == 3:
+        print("Среда")
+    elif x == 4:
+        print("Четверг")
+    elif x == 5:
+        print("Пятница")
+    elif x == 6:
+        print("Суббота")
+    elif x == 7:
+        print("Воскресенье")
 else:
     print("Нужно было ввести число от 1 - 7")
