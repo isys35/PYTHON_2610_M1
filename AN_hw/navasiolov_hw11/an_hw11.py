cc207dcecaacc7f9541d6c11d7b13a0175f5b354
iWERSAN <navasiolov.tolia@gmail.com>
2022-12-06 14:36:15 +0300

    fix_hw11

---
+++ AN_hw/navasiolov_hw11/an_hw11.py
@@ -0,0 +1,79 @@
+"""
+Задача 3.
+
+Напишите программу, в которой пользователь
+вводит целое число от 1 до 7 включительно,
+а программа выводит название дня недели,
+соответствующее этому числу
+("Понедельник" для 1, "Вторник " для 2, и так далее).
+"""
+
+
+def is_calendar(command: int):
+    """
+       Функция проверяет является ли введенные числа днями недели
+    :param command: "1", "2", "3", "4", "5", "6", "7"
+    :return: bool
+    """
+    if command == 1:
+        print("Понидельник")
+    elif command == 2:
+        print("фторник")
+    elif command == 3:
+        print("Среда")
+    elif command == 4:
+        print("Четверг")
+    elif command == 5:
+        print("Пятница")
+    elif command == 6:
+        print("Суббота")
+    elif command == 7:
+        print("Воскресенье")
+    else:
+        print("ОЩИБКА!")
+
+
+def main() -> is_calendar:
+    """ Точка входа"""
+    command = int(input("Введите число дня недели от 1 до 7 :"))
+    if is_calendar(command):
+        if command == 1:
+            print("Понидельник")
+        elif command == 2:
+            print("фторник")
+        elif command == 3:
+            print("Среда")
+        elif command == 4:
+            print("Четверг")
+        elif command == 5:
+            print("Пятница")
+        elif command == 6:
+            print("Суббота")
+        elif command == 7:
+            print("Воскресенье")
+        else:
+            print("ОЩИБКА!")
+
+
+if __name__ == "__main__":
+    main()
+
+    # Предыдущий вариант
+# command: = input("Введите число дня недели от 1 до 7 :")
+# match command:
+# case "1":
+# print("Понидельник")
+# case "2":
+# print("фторник")
+# case "3":
+# print("Среда")
+# case "4":
+#        print("Четверг")
+#    case "5":
+#        print("Пятница")
+#    case "6":
+#        print("Суббота")
+#    case "7":
+#        print("Воскресенье")
+# print("\n  Хорошего дня !")
+# привет
