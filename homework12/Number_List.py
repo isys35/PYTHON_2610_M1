00a4c0836eb7d6303cc3f590ddcd583c0a488c9b
Tatiana <tanyapilipshanova@gmail.com>
2022-12-18 18:09:29 +0300

    commit homework12

---
+++ homework12/Number_List.py
@@ -0,0 +1,34 @@
+"""
+# 8 Класс NumberList
+
+Напишите программу, в которой описан класс. У объектов класса должно быть поле, представляющее собой числовой список.
+Этот список формируется на основе списка, переданного конструктору в качестве аргумента. При этом из списка-аргумента
+в список-поле включаются только числовые элементы (элементы других типов игнорируются). Необходимо также описать метод,
+отображающий содержимое поля списка, а также метод, вычисляющий среднее значение элементов поля списка (сумма значений
+элементов, деленная на их количество).
+"""
+
+from statistics import mean
+from typing import Any, List
+
+
+class NumberList:
+
+    def __init__(self, data_list: List[Any]):
+        self.number_list = [x for x in data_list if isinstance(x, (float, int))]
+
+    def get_number_list(self) -> List:
+        return self.number_list
+
+    def mean_number_list(self) -> float:
+        return mean(self.number_list)
+
+
+def main():
+    data_list: NumberList = NumberList(["Hello!", 3, 6, 19, 2.5, "Dima"])
+    print(data_list.get_number_list())
+    print(data_list.mean_number_list())
+
+
+if __name__ == '__main__':
+    main()
